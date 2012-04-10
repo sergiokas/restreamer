21fc1b0769ee67a811626800c5605d5f4b7a2ab3
sergiokas <sergiokas@gmail.com>
2012-04-10 16:01:14 -0300

    removing old directory

--- restreamer/example.js
+++
@@ -1,3 +0,0 @@
-var restreamer = require("./lib/restreamer");
-
-var s = restreamer.create({port: 5566, override: true}).listen();
--- restreamer/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("./lib/restreamer-server");
