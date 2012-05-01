09bbba514f1645493bf7de94f90a35f16bddde3e
sergiokas <sergiokas@gmail.com>
2012-05-01 12:16:16 -0300

    adding port log

--- heroku.js
+++ heroku.js
@@ -1,3 +1,4 @@
 var restreamer = require("./lib/restreamer");
-
-var s = restreamer.create({port: process.env.PORT || 5566, override: true}).listen();
+var port = process.env.PORT || 5566;
+console.log('Listening on port ' + port);
+var s = restreamer.create({port: port, override: true}).listen();
